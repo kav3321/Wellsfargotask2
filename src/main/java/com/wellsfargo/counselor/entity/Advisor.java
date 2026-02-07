dba176e71867c834d894294966f4e55e6605c283
pchatman <34121076+pchatman@users.noreply.github.com>
2026-02-06 19:45:32 -0500

    chore: Create a new branch for task 2

--- src/main/java/com/wellsfargo/counselor/entity/Advisor.java
+++ src/main/java/com/wellsfargo/counselor/entity/Advisor.java
@@ -1,10 +1,9 @@
 package com.wellsfargo.counselor.entity;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class Advisor {
@@ -84,3 +83,4 @@ public class Advisor {
         this.email = email;
     }
 }
+
